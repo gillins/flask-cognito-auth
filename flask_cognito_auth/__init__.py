58aca19c4d9835d0f3e674070800729471a50e1d
Sam Gillingham <gillingham.sam@gmail.com>
2022-03-11 09:43:40 +1000

    import to top level

--- flask_cognito_auth/__init__.py
+++ flask_cognito_auth/__init__.py
@@ -2,3 +2,4 @@ from .cognito_auth_manager import CognitoAuthManager
 from .decorators import login_handler
 from .decorators import logout_handler
 from .decorators import callback_handler
+from .decorators import get_id_token_refresh_if_needed
