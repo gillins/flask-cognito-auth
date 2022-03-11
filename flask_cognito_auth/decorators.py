4a447447d4699bdbe2d6da3e66539735b49b8e95
Sam Gillingham <gillingham.sam@gmail.com>
2022-03-11 10:39:16 +1000

    add print statement

--- flask_cognito_auth/decorators.py
+++ flask_cognito_auth/decorators.py
@@ -248,6 +248,7 @@ def get_id_token_refresh_if_needed():
     """
     now = datetime.datetime.now(datetime.timezone.utc)
     if session['expires'] <= now:
+        print('refreshing token')
         request_parameters = {'grant_type': 'refresh_token',
                       'client_id': config.client_id,
                       "redirect_uri": config.redirect_uri,
