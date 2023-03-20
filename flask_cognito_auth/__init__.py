35deaf4c789cc7082a5fafd9b5adb2ea607cf45a
Sam Gillingham <gillingham.sam@gmail.com>
2023-03-21 08:10:57 +1000

    add import

--- flask_cognito_auth/__init__.py
+++ flask_cognito_auth/__init__.py
@@ -3,3 +3,4 @@ from .decorators import login_handler
 from .decorators import logout_handler
 from .decorators import callback_handler
 from .decorators import get_id_token_refresh_if_needed
+from .decorators import ReloginRequired
